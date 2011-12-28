75e888ae65a06ebbb5e43c05959d3fa8bc671565
Franco Bulgarelli <franco@zaubersoftware.com>
2011-12-28 16:53:01 -0300

    Adding tests for XmlgregorianCalendars

---
+++ src/test/java/org/mule/modules/utils/date/XmlGregorianCalendarsUnitTest.java
@@ -0,0 +1,50 @@
+/**
+ * Mule Modules Utils
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+
+package org.mule.modules.utils.date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.Calendar;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.junit.Test;
+
+public class XmlGregorianCalendarsUnitTest
+{
+
+    /**
+     * test for {@link XmlGregorianCalendars#nullSafeFrom(java.util.Date)}
+     */
+    @Test
+    public void testNullSafeFrom()
+    {
+        assertNull(XmlGregorianCalendars.nullSafeFrom(null));
+    }
+
+    /**
+     * test for {@link XmlGregorianCalendars#from(java.util.Date)}
+     */
+    @Test
+    public void testFrom()
+    {
+        Calendar calendar = Calendar.getInstance();
+
+        XMLGregorianCalendar xmlCalendar = XmlGregorianCalendars.from(calendar.getTime());
+
+        assertEquals(calendar.get(Calendar.DAY_OF_MONTH), xmlCalendar.getDay());
+        assertEquals(calendar.get(Calendar.MONTH) + 1, xmlCalendar.getMonth());
+        assertEquals(calendar.get(Calendar.HOUR_OF_DAY), xmlCalendar.getHour());
+    }
+
+}
